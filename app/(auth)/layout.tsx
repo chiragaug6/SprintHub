a12f6061938f81cfe292653dba0dc88a58a254ae
Chirag-Solanki6804 <crs279836@gmail.com>
2025-06-03 09:33:15 +0530

    Added User Authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return <div className="flex justify-center pt-20 pb-5">{children}</div>;
+};
+
+export default Layout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/header";
 import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +19,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          {/* header  */}
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <footer className="bg-gray-900 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made With 💖 by Chirag</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+          colorPrimary: "#3b82f6",
+          colorBackground: "#1a202c",
+          colorInputBackground: "#2D3748",
+          colorInputText: "#F3F4F6",
+        },
+        elements: {
+          formButtonPrimary: "text-white",
+          card: "bg-gray-800",
+          headerTitle: "text-blue-400",
+          headerSubtitle: "text-gray-400",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            {/* header  */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made With 💖 by Chirag</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
